0bc8b12e864f99a543af8d688dc2e921b372204d
Timofey Solonin <tim-solonin2@yandex.ru>
2018-03-16 19:50:26 +0300

    update

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,3 +1,4 @@
 yarn install
-yarn run ganache-cli --account="0x1636e10756e62baabddd4364010444205f1216bdb1644ff8f776f6e2982aa9f5,1" &
+nohup yarn run ganache-cli --account="0x1636e10756e62baabddd4364010444205f1216bdb1644ff8f776f6e2982aa9f5,1" &
 disown
+exit 0
