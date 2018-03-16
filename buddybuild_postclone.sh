8c7bf1369b130d5f535fd744bcc48b394290b58c
Timofey Solonin <tim-solonin2@yandex.ru>
2018-03-16 19:07:29 +0300

    updated

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,4 +1,2 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install yarn
-npm install -g ganache-cli
+yarn install
 yarn run ganache-cli --account="0x1636e10756e62baabddd4364010444205f1216bdb1644ff8f776f6e2982aa9f5,1" 
